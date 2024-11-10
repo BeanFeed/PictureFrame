05089f3eb70a9fcbd9a709c62bd23ef8ade6de05
BeanFeed <two.austin.two@gmail.com>
2024-11-10 12:50:51 -0500

    commit

--- UpdateServer/Services/BuildsService.cs
+++ UpdateServer/Services/BuildsService.cs
@@ -53,21 +53,20 @@ public class BuildsService
         string[] directories = Directory.GetDirectories(Path.Join(Environment.CurrentDirectory, "Builds"));
         if(directories.Length == 0) throw new Exception("No Builds Found.");
 
-        string newestBuild = directories[0];
+        if(!Utils.IsValidBuildNumber(directories[0])) throw new Exception("Invalid Build Number.");
+
+        DateTime newestBuild = new DateTime();
+
 
         for(int i = 1; i < directories.Length; i++)
         {
-            if(Utils.IsValidBuildNumber(directories[i]) && Utils.IsValidBuildNumber(newestBuild))
+            if(Utils.IsValidBuildNumber(directories[i]))
             {
-                string[] currentBuildParts = newestBuild.Split('.');
                 string[] newBuildParts = directories[i].Split('.');
-                if(int.Parse(currentBuildParts[0]) >= int.Parse(newBuildParts[0]) && int.Parse(currentBuildParts[1]) >= int.Parse(newBuildParts[1]) && int.Parse(currentBuildParts[2]) >= int.Parse(newBuildParts[2]) && int.Parse(currentBuildParts[3]) > int.Parse(newBuildParts[3]))
-                {
-                    newestBuild = directories[i];
-                }
+
             }
         }
 
-        return newestBuild;
+        return "";
     }
 }
